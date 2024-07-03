763da43f9ed60df92703772c3d819e6a98d602d7
aboutmydreams <aboutmydreams@163.com>
2024-07-04 01:49:11 +0800

    feat: add ncuhome GA4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,30 @@
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import type { AppProps } from 'next/app';
-import Script from 'next/script';
-import { Analytics } from '@vercel/analytics/react';
-import './style.css';
- 
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
+import "./style.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-ST7R3WQ353"/>
-<Script>{`
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-F99FVNWSL1"
+      />
+      <Script>
+        {`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-ST7R3WQ353');
+  gtag('config', 'G-F99FVNWSL1');
 `}
-</Script>
+      </Script>
 
       <Component {...pageProps} />
-     
+
       <Analytics />
     </>
-  
   );
 }
- 
+
 export default MyApp;
